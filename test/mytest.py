3e3d38bd5912a062783631717b819ea124af5447
tatsuya <tatsuya4649developer@gmail.com>
2021-07-01 11:12:51 +0900

    SELECT test

--- test/mytest.py
+++ test/mytest.py
@@ -10,19 +10,45 @@ def test_wrapper(f):
 		print("-----------------------")
 	return _wrapper
 
-def connection(host,user,passwd):
+def connection(host,user,passwd,database):
 	# Not Connect Specific Database
 	conn = pymysql.connect(
 		host=host,
 		user=user,
 		password=passwd,
+		database=database,
 		cursorclass=pymysql.cursors.DictCursor
 	)
 	return conn
 
+# SELECT TEST to MySQL Server
+def _select(conn,table,where):
+	sql = f"SELECT * FROM {table} WHERE {where}"
+	print(sql)
+	with conn.cursor() as cursor:
+		cursor.execute(sql)
+
+		results = cursor.fetchall()
+		for r in results:
+			print(r)
+	return
+@test_wrapper
+def select_test(host,user,passwd,database,table,where):
+	# new connection
+	conn = connection(host,user,passwd,database)
+	with conn:
+		try:
+			_select(conn,table,where)
+		except pymysql.err.OperationalError as e:
+			print(e)
+		else:
+			print("Select SUCCESS!")
+	return
 # PING TEST to MySQL Server
-def ping_test(conn):
-	# now connection
+@test_wrapper
+def ping_test(host,user,passwd,database):
+	# new connection
+	conn = connection(host,user,passwd,database)
 	with conn:
 		try:
 			res = conn.ping()
@@ -32,10 +58,9 @@ def ping_test(conn):
 			print("Ping SUCCESS!")
 	return
 
-@test_wrapper
-def test(host,user,passwd):
-	conn = connection(host,user,passwd)
-	ping_test(conn)
+def test(host,user,passwd,database,table,where):
+	ping_test(host,user,passwd,database)
+	select_test(host,user,passwd,database,table,where)
 
 
 if __name__ == "__main__":
@@ -45,4 +70,12 @@ if __name__ == "__main__":
 	_USER=os.environ['TESTUSER']
 	print(f"MySQL TEST SERVER USER NAME => {_USER}")
 	_PASSWORD=os.environ['TESTPASSWD']
-	test(_HOST,_USER,_PASSWORD)
+
+	_DATABASE=os.environ['TESTDATABASE']
+	print(f"MySQL TEST SERVER DATABASE NAME => {_DATABASE}")
+	_TABLE=os.environ['TESTTABLE']
+	print(f"MySQL TEST SERVER TABLE NAME => {_TABLE}")
+	_WHERE=os.environ['TESTWHERE']
+	_WHERE=_WHERE.replace('"','')
+	print(f"MySQL TEST SERVER TABLE CONDITION => {_WHERE}")
+	test(_HOST,_USER,_PASSWORD,_DATABASE,_TABLE,_WHERE)
