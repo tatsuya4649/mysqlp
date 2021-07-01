ac2935599f6184babd6c951540bb0abb5c7d6d9d
tatsuya <tatsuya4649developer@gmail.com>
2021-07-01 11:35:37 +0900

    test database & table

---
+++ schema/table.sql
@@ -0,0 +1,12 @@
+/*
+ *		this file is for making all 'TABLE'
+ *
+ */
+
+
+CREATE TABLE ex.user (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	comment VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
+);
